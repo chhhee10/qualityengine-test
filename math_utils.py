dcdef925bfeba9a42f51d9c2076e83329a7a3594
QualityEngine Demo <demo@qualityengine.ai>
2026-05-16 03:22:31 +0530

    Commiting a math function

--- math_utils.py
+++ math_utils.py
@@ -2,4 +2,4 @@ def add_numbers(a: int, b: int) -> int:
     """
     Adds two numbers and returns the result.
     """
-    return a * b  # BUG: This should be +
+    return a - b  # BUG: This should be +
